c6a0c4da1864aa0011e553662ced5e80905094fa
karott7 <karott7@gmail.com>
2022-04-11 22:24:47 +0800

    增加 parse 方法测试用例，并修复测试报错

---
+++ __tests__/parse.test.js
@@ -0,0 +1,400 @@
+const parse = require('../parse')
+
+describe('测试 parse 方法', () => {
+  it('是否是 XML 文档', () => {
+    expect(() => parse('123')).toThrow()
+  })
+
+  it('文档标记', () => {
+    const input = '<?xml version="1.0" encoding="utf-8"?>'
+    expect(
+      parse(input)
+    ).toEqual([{
+      type: 'DOCUMENT',
+      attrs: ['version="1.0"', 'encoding="utf-8"'],
+      start: 0,
+      end: input.length
+    }])
+  })
+
+  it('文档标记 + 评论', () => {
+    const input = '<?xml version="1.0" encoding="utf-8"?><!--comment-->'
+    expect(
+      parse(input)
+    ).toEqual([
+      {
+        type: 'DOCUMENT',
+        attrs: ['version="1.0"', 'encoding="utf-8"'],
+        start: 0,
+        end: 38
+      },
+      {
+        type: 'COMMENT',
+        value: 'comment',
+        start: 38,
+        end: input.length
+      }
+    ])
+  })
+
+  it('文档标记 + 评论 + 空元素', () => {
+    const input = '<?xml version="1.0" encoding="utf-8"?><!--comment--><a />'
+    expect(
+      parse(input)
+    ).toEqual([
+      {
+        type: 'DOCUMENT',
+        attrs: ['version="1.0"', 'encoding="utf-8"'],
+        start: 0,
+        end: 38
+      },
+      {
+        type: 'COMMENT',
+        value: 'comment',
+        start: 38,
+        end: 52
+      },
+      {
+        type: 'ELEMENT',
+        tag: 'a',
+        lowerCaseTagName: 'a',
+        start: 52,
+        end: input.length,
+        attrs: [],
+        children: [],
+        close: true,
+        isSelfClose: true
+      }
+    ])
+  })
+
+  it('文档标记 + 评论 + 空元素(带属性)', () => {
+    const input = '<?xml version="1.0" encoding="utf-8"?><!--comment--><a b="c" c=\'d\' />'
+    expect(
+      parse(input)
+    ).toEqual([
+      {
+        type: 'DOCUMENT',
+        attrs: ['version="1.0"', 'encoding="utf-8"'],
+        start: 0,
+        end: 38
+      },
+      {
+        type: 'COMMENT',
+        value: 'comment',
+        start: 38,
+        end: 52
+      },
+      {
+        type: 'ELEMENT',
+        tag: 'a',
+        lowerCaseTagName: 'a',
+        start: 52,
+        end: input.length,
+        attrs: ['b="c"', "c='d'"],
+        children: [],
+        close: true,
+        isSelfClose: true
+      }
+    ])
+  })
+
+  it('文档标记 + 评论 + 空元素 + 非空元素', () => {
+    const input = '<?xml version="1.0" encoding="utf-8"?><!--comment--><a /><B>b</B>'
+    expect(
+      parse(input)
+    ).toEqual([
+      {
+        type: 'DOCUMENT',
+        attrs: ['version="1.0"', 'encoding="utf-8"'],
+        start: 0,
+        end: 38
+      },
+      {
+        type: 'COMMENT',
+        value: 'comment',
+        start: 38,
+        end: 52
+      },
+      {
+        type: 'ELEMENT',
+        tag: 'a',
+        lowerCaseTagName: 'a',
+        start: 52,
+        end: 57,
+        attrs: [],
+        children: [],
+        close: true,
+        isSelfClose: true
+      },
+      {
+        type: 'ELEMENT',
+        tag: 'B',
+        lowerCaseTagName: 'b',
+        start: 57,
+        end: input.length,
+        attrs: [],
+        children: [
+          {
+            type: 'TEXT',
+            value: 'b',
+            start: 60,
+            end: 61
+          }
+        ],
+        close: true,
+        isSelfClose: false
+      }
+    ])
+  })
+
+  it('文档标记 + 评论 + 空元素 + 非空元素(带属性)', () => {
+    const input = '<?xml version="1.0" encoding="utf-8"?><!--comment--><a /><B c="d" e="f">b</B>'
+    expect(
+      parse(input)
+    ).toEqual([
+      {
+        type: 'DOCUMENT',
+        attrs: ['version="1.0"', 'encoding="utf-8"'],
+        start: 0,
+        end: 38
+      },
+      {
+        type: 'COMMENT',
+        value: 'comment',
+        start: 38,
+        end: 52
+      },
+      {
+        type: 'ELEMENT',
+        tag: 'a',
+        lowerCaseTagName: 'a',
+        start: 52,
+        end: 57,
+        attrs: [],
+        children: [],
+        close: true,
+        isSelfClose: true
+      },
+      {
+        type: 'ELEMENT',
+        tag: 'B',
+        lowerCaseTagName: 'b',
+        start: 57,
+        end: input.length,
+        attrs: ['c="d"', 'e="f"'],
+        children: [
+          {
+            type: 'TEXT',
+            value: 'b',
+            start: 72,
+            end: 73
+          }
+        ],
+        close: true,
+        isSelfClose: false
+      }
+    ])
+  })
+
+  it('文档标记 + 评论 + 空元素 + 非空元素 + 嵌套元素', () => {
+    const input = '<?xml version="1.0" encoding="utf-8"?><!--comment--><a /><B>b</B><c><d><e>e</e></d></c>'
+    expect(
+      parse(input)
+    ).toEqual([
+      {
+        type: 'DOCUMENT',
+        attrs: ['version="1.0"', 'encoding="utf-8"'],
+        start: 0,
+        end: 38
+      },
+      {
+        type: 'COMMENT',
+        value: 'comment',
+        start: 38,
+        end: 52
+      },
+      {
+        type: 'ELEMENT',
+        tag: 'a',
+        lowerCaseTagName: 'a',
+        start: 52,
+        end: 57,
+        attrs: [],
+        children: [],
+        close: true,
+        isSelfClose: true
+      },
+      {
+        type: 'ELEMENT',
+        tag: 'B',
+        lowerCaseTagName: 'b',
+        start: 57,
+        end: 65,
+        attrs: [],
+        children: [
+          {
+            type: 'TEXT',
+            value: 'b',
+            start: 60,
+            end: 61
+          }
+        ],
+        close: true,
+        isSelfClose: false
+      },
+      {
+        type: 'ELEMENT',
+        tag: 'c',
+        lowerCaseTagName: 'c',
+        start: 65,
+        end: 87,
+        attrs: [],
+        children: [
+          {
+            type: 'ELEMENT',
+            tag: 'd',
+            lowerCaseTagName: 'd',
+            start: 68,
+            end: 83,
+            attrs: [],
+            children: [
+              {
+                type: 'ELEMENT',
+                tag: 'e',
+                lowerCaseTagName: 'e',
+                start: 71,
+                end: 79,
+                attrs: [],
+                children: [
+                  {
+                    type: 'TEXT',
+                    value: 'e',
+                    start: 74,
+                    end: 75
+                  }
+                ],
+                close: true,
+                isSelfClose: false
+              },
+            ],
+            close: true,
+            isSelfClose: false
+          },
+        ],
+        close: true,
+        isSelfClose: false
+      },
+    ])
+  })
+
+  it('文档标记 + 评论 + 空元素 + 非空元素 + 嵌套多个元素', () => {
+    const input = '<?xml version="1.0" encoding="utf-8"?><!--comment--><a /><B>b</B><c><d><e>e</e><F>f</F></d></c>'
+    expect(
+      parse(input)
+    ).toEqual([
+      {
+        type: 'DOCUMENT',
+        attrs: ['version="1.0"', 'encoding="utf-8"'],
+        start: 0,
+        end: 38
+      },
+      {
+        type: 'COMMENT',
+        value: 'comment',
+        start: 38,
+        end: 52
+      },
+      {
+        type: 'ELEMENT',
+        tag: 'a',
+        lowerCaseTagName: 'a',
+        start: 52,
+        end: 57,
+        attrs: [],
+        children: [],
+        close: true,
+        isSelfClose: true
+      },
+      {
+        type: 'ELEMENT',
+        tag: 'B',
+        lowerCaseTagName: 'b',
+        start: 57,
+        end: 65,
+        attrs: [],
+        children: [
+          {
+            type: 'TEXT',
+            value: 'b',
+            start: 60,
+            end: 61
+          }
+        ],
+        close: true,
+        isSelfClose: false
+      },
+      {
+        type: 'ELEMENT',
+        tag: 'c',
+        lowerCaseTagName: 'c',
+        start: 65,
+        end: 95,
+        attrs: [],
+        children: [
+          {
+            type: 'ELEMENT',
+            tag: 'd',
+            lowerCaseTagName: 'd',
+            start: 68,
+            end: 91,
+            attrs: [],
+            children: [
+              {
+                type: 'ELEMENT',
+                tag: 'e',
+                lowerCaseTagName: 'e',
+                start: 71,
+                end: 79,
+                attrs: [],
+                children: [
+                  {
+                    type: 'TEXT',
+                    value: 'e',
+                    start: 74,
+                    end: 75
+                  }
+                ],
+                close: true,
+                isSelfClose: false
+              },
+              {
+                type: 'ELEMENT',
+                tag: 'F',
+                lowerCaseTagName: 'f',
+                start: 79,
+                end: 87,
+                attrs: [],
+                children: [
+                  {
+                    type: 'TEXT',
+                    value: 'f',
+                    start: 82,
+                    end: 83
+                  }
+                ],
+                close: true,
+                isSelfClose: false
+              },
+            ],
+            close: true,
+            isSelfClose: false
+          },
+        ],
+        close: true,
+        isSelfClose: false
+      },
+    ])
+  })
+})
+
---
+++ parse.js
@@ -0,0 +1,173 @@
+const DOCUMENT_TYPE = 'DOCUMENT'
+const ELEMENT_TYPE  = 'ELEMENT'
+const COMMENT_TYPE  = 'COMMENT'
+const TEXT_TYPE     = 'TEXT'
+
+const DOCTYPE = /^<\?xml/
+const COMMENT = /^<!--(.*)-->/
+const COMMENT_CLOSE = /-->/
+const ATTR = /^[\s\r\n]*[a-zA-Z][a-zA-Z0-9:]*(?:=(?:"[^"]*"|'[^']*'))/
+const TAG_START_OPEN = /^<([a-zA-Z][a-zA-Z0-9:-]*)/
+const TAG_START_CLOSE = /^[\s\n]*(\/)?>/
+const TAG_END = /^<\/([a-zA-Z][a-zA-Z0-9]*[^>]*)>/
+
+function parse(template) {
+  if (!DOCTYPE.test(template)) {
+    throw new Error('请输入 xml 格式字符串')
+  }
+
+  const tokens = []
+  const stack = []
+  let index = 0, lastTag, lastTemplate
+
+  while (template) {
+    if (lastTemplate === template) {
+      throw new Error(`模版重复：${template}`)
+    }
+    lastTemplate = template
+
+    const i = template.indexOf('<')
+    let matched, end, parent = stack[stack.length - 1]
+    if (i === 0) {
+      // 节点处理
+      if (matched = template.match(DOCTYPE)) {
+        const element = {
+          type: DOCUMENT_TYPE,
+          attrs: [],
+          start: index,
+        }
+
+        forward(matched[0].length)
+
+        while (!(end = template.match(/^[\s\n]*\?>/)) && (matched = template.match(ATTR))) {
+          element.attrs.push(matched[0].trim())
+          forward(matched[0].length)
+          continue
+        }
+
+        if (end) {
+          element.end = index + end[0].length
+          forward(end[0].length)
+        }
+
+        tokens.push(element)
+        continue
+      }
+
+      if (matched = template.match(COMMENT)) {
+        if (matched[1].trim()) {
+          const element = {
+            type: COMMENT_TYPE,
+            value: matched[1].trim(),
+            start: index,
+            end: index + matched[0].length,
+          }
+
+          if (parent) {
+            parent.children.push(element)
+          } else {
+            tokens.push(element)
+          }
+        }
+
+        forward(matched[0].length)
+        continue
+      }
+
+      if (matched = template.match(TAG_END)) {
+        const el = stack[stack.length - 1]
+        if (el) {
+          el.end = index + matched[0].length
+          el.close = true
+          stack.pop()
+
+          parent = stack[stack.length - 1]
+          if (parent) {
+            parent.children.push(el)
+          } else {
+            tokens.push(el)
+          }
+
+          forward(matched[0].length)
+        } else {
+          throw new Error('no matched element')
+        }
+
+        continue
+      }
+
+      if (matched = template.match(TAG_START_OPEN)) {
+        const element = {
+          type: ELEMENT_TYPE,
+          tag: matched[1],
+          lowerCaseTagName: matched[1].toLowerCase(),
+          start: index,
+          attrs: [],
+          children: [],
+          close: false,
+          isSelfClose: false
+        }
+
+        forward(matched[0].length)
+
+        while (!(end = template.match(TAG_START_CLOSE)) && (matched = template.match(ATTR))) {
+          element.attrs.push(matched[0].trim())
+          forward(matched[0].length)
+          continue
+        }
+
+        if (end && end[1] === '/') {
+          element.isSelfClose = true
+          element.close = true
+          element.end = index + end[0].length
+          parent = stack[stack.length - 1]
+          if (parent) {
+            parent.children.push(element)
+          } else {
+            tokens.push(element)
+          }
+        } else {
+          stack.push(element)
+          lastTag = element.tag
+        }
+
+        forward(end[0].length)
+        continue
+      }
+    }
+
+    if (/^(\n|\s)/.test(template)) {
+      forward(1)
+      continue
+    }
+
+    const endTagRe = new RegExp('^([\\s\\S]*)<\\/(' + lastTag + ')>', 'i')
+    let rest = template.replace(endTagRe, (all, text, endTag) => {
+      if (text && text.trim()) {
+        const element = {
+          type: TEXT_TYPE,
+          value: text.trim(),
+          start: index,
+          end: index + text.length
+        }
+
+        parent.children.push(element)
+
+        forward(text.length)
+      }
+
+      return ''
+    })
+  }
+
+  function forward(length) {
+    template = template.slice(length)
+    index += length
+  }
+
+  return tokens
+}
+
+// const tokens = parse(xml.trim())
+
+module.exports = parse
